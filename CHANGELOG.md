e4e4d7a3f5a37ec4d9865b136ebf07d53ff54268
lisen <lisen70708>
2019-10-15 17:56:47 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## [0.0.1] 
+* Initial Open Sources
+* Support Android iOS
+* Handling the original flutter print incomplete, no json format problem
+
+
---
+++ lib/Printer.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+class Printer {
+  static bool enable = true;
+
+  static void printLog(dynamic content) {
+    if (enable) {
+      _warp(content.toString());
+    }
+  }
+
+  //
+  static void printMapJsonLog(dynamic content, {State state, int lineNum}) {
+    if (enable) {
+      _warpMapJson(content, state: state, lineNum: lineNum);
+    }
+  }
+
+  static void _warpJson(String msg) {
+//    print(
+//        "┌------------------------------------------------------------------------------------------------------------------------------------------┐");
+    if (!msg.contains("{") && !msg.contains("}")) {
+      print("   "+msg);
+    } else {
+      RegExp regExp = RegExp(r"{|}|\[|,");
+      String rs = msg.replaceAllMapped(regExp, (Match m) => "${m[0]}\n");
+      //    print(rs);
+      String rs1 = rs.substring(0, rs.length - 2) + "\n}";
+      //    print(rs1);
+      String rsResult = rs1.replaceAllMapped(RegExp("\n"), (Match m) {
+        return "${m[0]}\t\t";
+      });
+      print("\t\t" + rsResult.substring(0, rsResult.length - 1) + "}");
+    }
+//    print(
+//        "└------------------------------------------------------------------------------------------------------------------------------------------┘");
+  }
+
+  //带缩进
+  static void _warpMapJson(dynamic msg, {State state, int lineNum: 0}) {
+    String top =
+        "┌------------------------------------------------------------------------------------------------------------------------------------------┐";
+    String bottom =
+        "└------------------------------------------------------------------------------------------------------------------------------------------┘";
+    //当前State ---->
+    String stateName = "StateName ----> " +
+        state.toString().split("#")[0].toString() +
+        ", lineNum ----> " +
+        lineNum.toString();
+    top = top.replaceRange(10, stateName.length, stateName);
+    bottom = bottom.replaceRange(10, stateName.length, stateName);
+    print(top);
+//    Map<String, dynamic> map = msg;
+    _genReadableJson(msg);
+
+    print(bottom);
+  }
+
+  static void _genReadableJson(dynamic data) {
+    if (data is List) {
+      _resolveList(1, data, false);
+    } else if (data is Map) {
+      _resolveMap(1, data);
+    } else {
+      if (data == null) {
+        print("   null");
+      } else {
+        _warpJson(data.toString());
+      }
+    }
+  }
+
+  static void _resolveMap(int space, Map<dynamic, dynamic> data) {
+    int index = 0;
+    space++;
+    StringBuffer stringBufferStart = new StringBuffer();
+    stringBufferStart.write("|");
+    for (int i = 0; i < space; i++) {
+      stringBufferStart.write("\t");
+    }
+    stringBufferStart.write("{\n");
+    print(stringBufferStart.toString());
+    data.forEach((key, value) {
+      index++;
+      StringBuffer stringBuffer = new StringBuffer();
+      stringBuffer.write("|");
+      for (int i = 0; i < space + 1; i++) {
+        stringBuffer.write("\t");
+      }
+      stringBuffer.write("\"" + key + "\"");
+      stringBuffer.write(":");
+      if (value is Map) {
+        print(stringBuffer.toString());
+//        space ++;
+        _resolveMap(space, value);
+        if (index == data.length - 1) {
+          StringBuffer stringBuffer = new StringBuffer();
+          stringBuffer.write("|");
+          for (int i = 0; i < space + 1; i++) {
+            stringBuffer.write("\t");
+          }
+          stringBuffer.write(",");
+          print(stringBuffer.toString());
+        }
+      } else if (value is List) {
+        print(stringBuffer.toString());
+        space++;
+        _resolveList(space, value, index != data.length);
+      } else {
+        var v = value;
+        if (v == null) {
+          stringBuffer.write("null");
+        } else {
+          stringBuffer.write("\"" + value.toString() + "\"");
+        }
+        if (index != data.length) {
+          stringBuffer.write(",");
+        }
+        stringBuffer.write("\n");
+        print(stringBuffer.toString());
+      }
+    });
+
+    StringBuffer stringBufferEnd = new StringBuffer();
+    stringBufferEnd.write("|");
+    for (int i = 0; i < space; i++) {
+      stringBufferEnd.write("\t");
+    }
+    stringBufferEnd.write("}");
+    if (index == data.length) {
+//      stringBufferEnd.write(",======");
+    }
+
+    print(stringBufferEnd.toString());
+  }
+
+  static void _resolveList(int space, List list, bool isLast) {
+    int index = 0;
+    space++;
+    StringBuffer stringBufferStart = new StringBuffer();
+    stringBufferStart.write("|");
+    for (int i = 0; i < space; i++) {
+      stringBufferStart.write("\t");
+    }
+    stringBufferStart.write("[\n");
+    print(stringBufferStart.toString());
+    list.forEach((item) {
+      index++;
+      if (item is Map) {
+        _resolveMap(space, item);
+        if (index != list.length) {
+          StringBuffer stringBufferMapEnd = new StringBuffer();
+          stringBufferMapEnd.write("|");
+          for (int i = 0; i < space + 1; i++) {
+            stringBufferMapEnd.write("\t");
+          }
+          stringBufferMapEnd.write(",");
+          print(stringBufferMapEnd.toString());
+        }
+      } else if (item is List) {
+        space++;
+        _resolveList(space, item, isLast);
+      } else {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.write("|");
+        for (int i = 0; i < space; i++) {
+          stringBuffer.write("\t");
+        }
+        stringBuffer.write("\"" + item.toString() + "\"");
+        stringBuffer.write(",");
+        print(stringBuffer.toString());
+      }
+    });
+    StringBuffer stringBufferEnd = new StringBuffer();
+    stringBufferEnd.write("|");
+    for (int i = 0; i < space; i++) {
+      stringBufferEnd.write("\t");
+    }
+    stringBufferEnd.write("]");
+    if (isLast) {
+      stringBufferEnd.write(",");
+    }
+    print(stringBufferEnd.toString());
+  }
+
+  static void _warp(String msg) {
+    print(
+        "┌------------------------------------------------------------------------------------------------------------------------------------------┐");
+    RegExp regExp = RegExp(r"{|}|\[");
+    String rs = msg.replaceAllMapped(regExp, (Match m) => "${m[0]}\n");
+    print("│\t" + rs);
+    print(
+        "└------------------------------------------------------------------------------------------------------------------------------------------┘");
+  }
+}
