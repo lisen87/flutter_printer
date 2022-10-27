c5efba5cbaf5ae507b10feefcf877d9013835cfb
lisen <453354858@qq.com>
2022-10-27 17:35:34 +0800

    增加打印颜色

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_printer/flutter_printer.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -32,27 +33,85 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
 
-      List list = ["http://xxxx0.png","http://xxxx1.png","http://xxxx2.png","http://xxxx3.png"];
-      Map map1 = {"id":"1"};
-      List list1 = [map1,map1,];
-      List list2 = [list1,null,list1,[map1,map1,map1,map1],[list1,list1,list1,[],list1,]];
+      List list = [
+        "http://xxxx0.png",
+        "http://xxxx1.png",
+        "http://xxxx2.png",
+        "http://xxxx3.png"
+      ];
+      Map map1 = {"id": "1"};
+      List list1 = [
+        map1,
+        map1,
+      ];
+      List list2 = [
+        list1,
+        null,
+        list1,
+        [map1, map1, map1, map1],
+        [list1, list1, list1, [], list1]
+      ];
 
-      Map map = {"map" : {"key1" : 1,"key2" : "2","list2" : []},"id":"1","orders":null,"member":{"id":"888","name":"leeson","phone":null,"age" : 88,"last" : list},"last" : list};
+      Map map = {
+        "map": {"key1": 1, "key2": "2", "list2": []},
+        "id": "1",
+        "orders": null,
+        "member": {
+          "id": "888",
+          "name": "leeson",
+          "phone": null,
+          "age": 88,
+          "last": list
+        },
+        "last": list
+      };
 
-      Printer.printMapJsonLog(map1,stackTrace: StackTrace.current,prefix: "我是测试前缀:",);
-      Printer.printMapJsonLog(list,stackTrace: StackTrace.current,);
-      Printer.printMapJsonLog(list2,stackTrace: StackTrace.current,);
-      Printer.printMapJsonLog(map,stackTrace: StackTrace.current,);
+
+      Printer.printMapJsonLog(
+        map1,
+        stackTrace: StackTrace.current,
+        prefix: "我是测试前缀:",
+      );
+      Printer.debug(
+        list,
+        stackTrace: StackTrace.current,
+      );
+      Printer.warn(
+        list2,
+        stackTrace: StackTrace.current,
+      );
+      Printer.error(
+        map,
+        stackTrace: StackTrace.current,
+      );
+
 //
 //      ///不推荐的使用方式
 //      /// Not recommended
 //      Printer.printMapJsonLog("printer--> Printer.printLog"+map.toString(),stackTrace: StackTrace.current,);
 
-      String data = "BuildContext\nBuildContext\nBuildContext\nBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContext";
+      String data =
+          "BuildContext\nBuildContext\nBuildContext\nBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContextBuildContext";
       // Printer.printMapJsonLog(null,stackTrace: StackTrace.current);
       // Printer.printMapJsonLog("",stackTrace: StackTrace.current);
-      Printer.printMapJsonLog(data,stackTrace: StackTrace.current);
+      Printer.printMapJsonLog(data, stackTrace: StackTrace.current);
+
+      Printer.info(data,
+          stackTrace: StackTrace.current,
+          prefix: "我是测试前缀:",
+          xtermRgb: xtermRgb(
+              Colors.white.red, Colors.white.green, Colors.white.blue));
     });
+  }
+  @override
+  void initState() {
+    super.initState();
+    // Printer.config = ColorConfig(
+    //   infoRgb: xtermRgb(Colors.red.red, Colors.red.green, Colors.red.blue),
+    //   debugRgb: xtermRgb(Colors.orange.red, Colors.orange.green, Colors.orange.blue),
+    //   warnRgb: xtermRgb(Colors.brown.red, Colors.brown.green, Colors.brown.blue),
+    //   errorRgb: xtermRgb(Colors.teal.red, Colors.teal.green, Colors.teal.blue),
+    // );
   }
 
   @override
--- lib/flutter_printer.dart
+++ lib/flutter_printer.dart
@@ -1,25 +1,60 @@
 class Printer {
   ///是否开启打印 Whether to turn on printing
-
   static bool enable = true;
+
+  static ColorConfig config  = ColorConfig();
 
   ///Print map or list, etc.
   ///stackTrace stackTrace
   ///prefix prefix
+  ///xtermRgb 0-255
   ///可已打印任意数据，stackTrace不为null时可显示出使用Printer的位置，prefix 打印数据前缀
   ///Any data can be printed. When the stackTrace is not null, the location where the printer is used can be displayed. The prefix prints the data prefix.
-
   static void printMapJsonLog(dynamic content,
-      {StackTrace? stackTrace, String? prefix}) {
+      {StackTrace? stackTrace, String? prefix,int? xtermRgb}) {
     if (enable) {
-      _warpMapJson(content, stackTrace: stackTrace, prefix: prefix);
+      if(xtermRgb == null){
+        xtermRgb = config.infoRgb;
+      }
+      _warpMapJson(content, stackTrace: stackTrace, prefix: prefix,xtermRgb:xtermRgb);
     }
   }
 
+  static void info(dynamic content,
+      {StackTrace? stackTrace, String? prefix,int? xtermRgb}){
+    if(xtermRgb == null){
+      xtermRgb = config.infoRgb;
+    }
+    printMapJsonLog(content,stackTrace: stackTrace,prefix: prefix,xtermRgb: xtermRgb);
+  }
+
+  static void debug(dynamic content,
+      {StackTrace? stackTrace, String? prefix,int? xtermRgb}){
+    if(xtermRgb == null){
+      xtermRgb = config.debugRgb;
+    }
+    printMapJsonLog(content,stackTrace: stackTrace,prefix: prefix,xtermRgb: xtermRgb);
+  }
+
+  static void warn(dynamic content,
+      {StackTrace? stackTrace, String? prefix,int? xtermRgb}){
+    if(xtermRgb == null){
+      xtermRgb = config.warnRgb;
+    }
+    printMapJsonLog(content,stackTrace: stackTrace,prefix: prefix,xtermRgb: xtermRgb);
+  }
+
+  static void error(dynamic content,
+      {StackTrace? stackTrace, String? prefix,int? xtermRgb}){
+    if(xtermRgb == null){
+      xtermRgb = config.errorRgb;
+    }
+    printMapJsonLog(content,stackTrace: stackTrace,prefix: prefix,xtermRgb: xtermRgb);
+  }
 
   ///With indentation
   static void _warpMapJson(dynamic msg,
-      {StackTrace? stackTrace, String? prefix}) {
+      {StackTrace? stackTrace, String? prefix,int? xtermRgb = 10}) {
     String top =
         "┌------------------------------------------------------------------------------------------------------------------------------------------┐";
     String bottom =
@@ -33,192 +68,225 @@ class Printer {
       top = top.replaceRange(10, stackInfo.length, stackInfo);
       bottom = bottom.replaceRange(10, stackInfo.length, stackInfo);
     }
-    print(top);
+    _print(top,xtermRgb);
     if (prefix != null && prefix.trim().length != 0) {
-      print("|\t" + prefix);
+      _print('|\t' + prefix,xtermRgb);
     }
-    _genReadableJson(msg);
+    _genReadableJson(msg,xtermRgb);
 
-    print(bottom);
+    _print(bottom,xtermRgb);
   }
 
   ///Format json data by type
-  static void _genReadableJson(dynamic data) {
+  static void _genReadableJson(dynamic data,int? xtermRgb) {
     if (data is List) {
-      _resolveList(1, data, false);
+      _resolveList(1, data, false,xtermRgb);
     } else if (data is Map) {
-      _resolveMap(1, data, false);
+      _resolveMap(1, data, false,xtermRgb);
     } else {
       if(data is String){
-        _splitStr(1,data);
+        _splitStr(1,data,xtermRgb);
       }else{
-        print("|\t"+ data.toString());
+        _print('|\t'+ data.toString(),xtermRgb);
       }
     }
   }
 
-  static _splitStr(int space ,String? data){
-    if(data != null && data.contains("\n")){
+  static _splitStr(int space ,String? data,int? xtermRgb){
+    if(data != null && data.contains('\n')){
       StringBuffer stringBuffer = StringBuffer();
-      List<String> list = data.split(RegExp(r"\n"));
-      stringBuffer.write("|");
+      List<String> list = data.split(RegExp(r'\n'));
+      stringBuffer.write('|');
       for (int i = 0; i < space ; i++) {
-        stringBuffer.write("\t");
+        stringBuffer.write('\t');
       }
       list.forEach((str){
-        _splitLineStr(space,str);
+        _splitLineStr(space,str,xtermRgb);
       });
     }else{
-      _splitLineStr(space,data);
+      _splitLineStr(space,data,xtermRgb);
     }
   }
 
-  static _splitLineStr(int space ,String? data) {
+  static _splitLineStr(int space ,String? data,int? xtermRgb) {
     StringBuffer stringBuffer = StringBuffer();
     int limit = 150;
     if (data != null && data.length > limit) {
       int count = data.length ~/ limit;
-      stringBuffer.write("|");
+      stringBuffer.write('|');
       for (int i = 0; i < space; i++) {
-        stringBuffer.write("\t");
+        stringBuffer.write('\t');
       }
       for (int i = 0; i < count; i++) {
         if (i == count - 1) {
-          print(
-              stringBuffer.toString() + data.substring(i * limit, data.length));
+          _print(
+              stringBuffer.toString() + data.substring(i * limit, data.length),xtermRgb);
         } else {
-          print(stringBuffer.toString() +
-              data.substring(i * limit, i * limit + limit));
+          _print(stringBuffer.toString() +
+              data.substring(i * limit, i * limit + limit),xtermRgb);
         }
       }
     }else{
-      print("|\t"+ data!);
+      _print('|\t'+ data!,xtermRgb);
     }
   }
 
   ///Format json
-  static void _resolveMap(int space, Map<dynamic, dynamic> data, bool isLast) {
+  static void _resolveMap(int space, Map<dynamic, dynamic> data, bool isLast,int? xtermRgb) {
     int index = 0;
     space++;
     StringBuffer stringBufferStart = new StringBuffer();
-    stringBufferStart.write("|");
+    stringBufferStart.write('|');
     for (int i = 0; i < space; i++) {
-      stringBufferStart.write("\t");
+      stringBufferStart.write('\t');
     }
-    stringBufferStart.write("{\n");
-    print(stringBufferStart.toString());
+    // stringBufferStart.write("{\n");
+    stringBufferStart.write('{');
+    _print(stringBufferStart.toString(),xtermRgb);
     data.forEach((key, value) {
       index++;
       StringBuffer stringBuffer = new StringBuffer();
-      stringBuffer.write("|");
+      stringBuffer.write('|');
       for (int i = 0; i < space + 1; i++) {
-        stringBuffer.write("\t");
+        stringBuffer.write('\t');
       }
-      stringBuffer.write("\"" + key + "\"");
-      stringBuffer.write(" : ");
+      stringBuffer.write('\"' + key + '\"');
+      stringBuffer.write(' : ');
       if (value is Map) {
-        print(stringBuffer.toString());
-        _resolveMap(space + 1, value, index != data.length);
+        _print(stringBuffer.toString(),xtermRgb);
+        _resolveMap(space + 1, value, index != data.length,xtermRgb);
       } else if (value is List) {
         if (value.length == 0) {
-          stringBuffer.write("[]");
+          stringBuffer.write('[]');
           if(index != data.length){
-            stringBuffer.write(",");
+            stringBuffer.write(',');
           }
-          print(stringBuffer.toString());
+          _print(stringBuffer.toString(),xtermRgb);
         } else {
-          print(stringBuffer.toString());
-          _resolveList(space + 1, value, index != data.length);
+          _print(stringBuffer.toString(),xtermRgb);
+          _resolveList(space + 1, value, index != data.length,xtermRgb);
         }
       } else {
         if (value == null) {
           stringBuffer.write(null);
         } else {
           if (value is String) {
-            stringBuffer.write("\"" + value.toString() + "\"");
+            stringBuffer.write('\"' + value.toString() + '\"');
           } else {
             stringBuffer.write(value);
           }
         }
         if (index != data.length) {
-          stringBuffer.write(",");
+          stringBuffer.write(',');
         }
-        stringBuffer.write("\n");
-        print(stringBuffer.toString());
+        // stringBuffer.write("\n");
+        _print(stringBuffer.toString(),xtermRgb);
       }
     });
 
     StringBuffer stringBufferEnd = new StringBuffer();
-    stringBufferEnd.write("|");
+    stringBufferEnd.write('|');
     for (int i = 0; i < space; i++) {
-      stringBufferEnd.write("\t");
+      stringBufferEnd.write('\t');
     }
-    stringBufferEnd.write("}");
+    stringBufferEnd.write('}');
     if (isLast) {
-      stringBufferEnd.write(",");
+      stringBufferEnd.write(',');
     }
 
-    print(stringBufferEnd.toString());
+    _print(stringBufferEnd.toString(),xtermRgb);
   }
 
   ///Format list json
-  static void _resolveList(int space, List? list, bool isLast) {
+  static void _resolveList(int space, List? list, bool isLast,int? xtermRgb) {
     int index = 0;
     space++;
     StringBuffer stringBufferStart = new StringBuffer();
-    stringBufferStart.write("|");
+    stringBufferStart.write('|');
     for (int i = 0; i < space; i++) {
-      stringBufferStart.write("\t");
+      stringBufferStart.write('\t');
     }
 
     if (list == null) {
       stringBufferStart.write(null);
-      print(stringBufferStart.toString());
+      _print(stringBufferStart.toString(),xtermRgb);
     } else {
       if (list.length == 0) {
-        stringBufferStart.write("[]");
+        stringBufferStart.write('[]');
         if (isLast) {
-          stringBufferStart.write(",");
+          stringBufferStart.write(',');
         }
-        print(stringBufferStart.toString());
+        _print(stringBufferStart.toString(),xtermRgb);
       } else {
-        stringBufferStart.write("[");
-        print(stringBufferStart.toString());
+        stringBufferStart.write('[');
+        _print(stringBufferStart.toString(),xtermRgb);
         list.forEach((item) {
           index++;
           if (item is Map) {
-            _resolveMap(space, item, index != list.length);
+            _resolveMap(space, item, index != list.length,xtermRgb);
           } else if (item is List) {
-            _resolveList(space + 1, item, index != list.length);
+            _resolveList(space + 1, item, index != list.length,xtermRgb);
           } else {
             StringBuffer stringBuffer = new StringBuffer();
-            stringBuffer.write("|");
+            stringBuffer.write('|');
             for (int i = 0; i < space + 1; i++) {
-              stringBuffer.write("\t");
+              stringBuffer.write('\t');
             }
             if (item is String) {
-              stringBuffer.write("\"" + item.toString() + "\"");
+              stringBuffer.write('\"' + item.toString() + '\"');
             } else {
               stringBuffer.write(item);
             }
             if (index != list.length) {
-              stringBuffer.write(",");
+              stringBuffer.write(',');
             }
-            print(stringBuffer.toString());
+            _print(stringBuffer.toString(),xtermRgb);
           }
         });
         StringBuffer stringBufferEnd = new StringBuffer();
-        stringBufferEnd.write("|");
+        stringBufferEnd.write('|');
         for (int i = 0; i < space; i++) {
-          stringBufferEnd.write("\t");
+          stringBufferEnd.write('\t');
         }
-        stringBufferEnd.write("]");
+        stringBufferEnd.write(']');
         if (isLast) {
-          stringBufferEnd.write(",");
+          stringBufferEnd.write(',');
         }
-        print(stringBufferEnd.toString());
+        _print(stringBufferEnd.toString(),xtermRgb);
       }
     }
   }
 
+  static _print(Object? object,int? xtermRgb){
+    print('\x1B[38;5;${xtermRgb}m$object\x1B[0m');
+  }
+}
+
+/// xterm color 256
+/// ColorConfig(infoRgb:xtermRgb(Colors.red.red,Colors.red.green,Colors.red.blue));
+class ColorConfig{
+  /// xterm color
+  final int? infoRgb;
+  /// xterm color
+  final int? debugRgb;
+  /// xterm color
+  final int? warnRgb;
+  /// xterm color
+  final int? errorRgb;
+
+  ColorConfig({this.infoRgb = 10, this.debugRgb = 14, this.warnRgb = 11, this.errorRgb = 9});
+}
+
+/// red 0-255
+///
+/// red green-255
+///
+/// red blue-255
+///
+/// return xterm color
+int xtermRgb(int red, int green, int blue) {
+  return ((red / 255).clamp(0.0, 1.0) * 5).toInt() * 36 +
+      ((green / 255).clamp(0.0, 1.0) * 5).toInt() * 6 +
+      ((blue / 255).clamp(0.0, 1.0) * 5).toInt() +
+      16;
 }
